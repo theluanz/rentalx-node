673642caba51dae58bc95bd23cf9cbffb3c6b83d
theluanz <luanzuffo.lz@gmail.com>
2022-07-26 20:59:04 -0300

    Create user

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
+import { User } from "../modules/accounts/entities/User";
 import { Category } from "../modules/cars/entities/Category";
 import { Specification } from "../modules/cars/entities/Specification";
 
@@ -11,7 +12,7 @@ const AppDataSource = new DataSource({
   username: "postgres",
   password: "mysecretpassword",
   database: "rentx_db",
-  entities: [Category, Specification],
+  entities: [Category, Specification, User],
   migrations: ["./src/database/migrations/*.ts"],
 });
 
---
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -0,0 +1,11 @@
+import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../entities/User";
+
+interface IUsersRepository {
+  create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User | undefined>;
+  findById(id: string): Promise<User | undefined>;
+  save(user: User): Promise<void>;
+}
+
+export { IUsersRepository };
